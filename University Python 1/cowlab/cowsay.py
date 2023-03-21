728b381520c92c7360d455df7dc6fb221de16f03
GeoffreyFClark <97141856+GeoffreyFClark@users.noreply.github.com>
2023-03-21 15:59:04 -0600

    update

---
+++ University Python 1/cowlab/cowsay.py
@@ -0,0 +1,28 @@
+import sys
+from heifer_generator import HeiferGenerator
+from cow import Cow
+
+
+def main():
+    cow_objects = HeiferGenerator.get_cows()
+    if len(sys.argv) == 2 and sys.argv[1] == '-l':
+        cow_names = [cow.get_name() for cow in cow_objects]
+        print(f"Cows available: {' '.join(cow_names)}")
+    elif len(sys.argv) > 3 and sys.argv[1] == '-n':
+        cow_name = sys.argv[2]
+        cow_image = next((cow.get_image() for cow in cow_objects if cow.get_name() == cow_name), None)
+        if cow_image is not None:
+            cow_message = ' '.join(sys.argv[3:])
+            print(cow_message)
+            print(cow_image)
+        else:
+            print(f"Could not find {cow_name} cow!")
+
+    else:
+        cow_message = ' '.join(sys.argv[1:])
+        print(cow_message)
+        print(cow_objects[0].get_image())
+
+
+if __name__ == '__main__':
+    main()
